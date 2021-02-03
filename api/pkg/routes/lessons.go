d6acca53cc7778932b53ea49ebb3f7bd96881e2c
ocanty <git@ocanty.com>
2021-02-03 02:36:18 +0000

    basic stub routes for lessons

--- api/pkg/routes/lessons.go
+++ api/pkg/routes/lessons.go
@@ -1,9 +1,16 @@
 package routes
 
 import (
+	"net/http"
+
 	"github.com/gorilla/mux"
 )
 
 func InjectLessonsRoutes(subrouter *mux.Router) {
+	subrouter.PathPrefix("/{uuid}").Methods("GET").HandlerFunc(handleLessonGet)
+
+}
+
+func handleLessonGet(W http.ResponseWriter, r *http.Request) {
 
 }
