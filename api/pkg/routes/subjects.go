c217fe79c35ed5f202b5f9dc7d3e09be700b7f6f
RaymondReynolds <118408492@umail.ucc.ie>
2021-02-06 18:34:11 +0000

    Subjects stuff - needs testing and edits in places

--- api/pkg/routes/subjects.go
+++ api/pkg/routes/subjects.go
@@ -1,9 +1,128 @@
 package routes
 
 import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/cs3305-team-4/api/pkg/services"
+	"github.com/google/uuid"
 	"github.com/gorilla/mux"
 )
 
 func InjectSubjectsRoutes(subrouter *mux.Router) {
+	subrouter.HandleFunc("", handleSubjectsGet).Methods("GET")
+	subrouter.HandleFunc("/tutors", handleSubjectTutorsGet).Methods("GET") //.Queries("filter", "{filter}")
+}
+
+//Subject DTO represents an existing subject
+type Subject struct {
+	Name string    `json:"name"`
+	Slug string    `json:"slug"`
+	ID   uuid.UUID `json:"subject_id"`
+}
+
+//TutorSubjectDTO
+type TutorSubject struct {
+	SubjectName    string    `json:"subject_name"`
+	SubjectID      uuid.UUID `json:"subject_id"`
+	TutorFirstName string    `json:"tutor_first_name"`
+	TutorLastName  string    `json:"tutor_last_name"`
+	TutorAccountID uuid.UUID `json:"tutor_id"`
+	Price          uint      `json:"price"`
+	Description    string    `json:"description"`
+}
+
+func SingleTutorSubjectToDTO(subjectTaught *services.SubjectTaught) *TutorSubject {
+	return &TutorSubject{
+		SubjectName:    subjectTaught.Subject.Name,
+		SubjectID:      subjectTaught.Subject.ID,
+		Price:          subjectTaught.Price,
+		Description:    subjectTaught.Description,
+		TutorFirstName: subjectTaught.Tutor.Profile.FirstName,
+		TutorLastName:  subjectTaught.Tutor.Profile.LastName,
+		TutorAccountID: subjectTaught.Tutor.ID,
+	}
+
+}
+
+func TutorSubjectsToDTO(tutorSubjects []services.SubjectTaught) []TutorSubject {
+	tutorSubjectsDTO := []TutorSubject{}
+	for _, item := range tutorSubjects {
+		tutorSubjectsDTO = append(tutorSubjectsDTO, *SingleTutorSubjectToDTO(&item))
+	}
+	return tutorSubjectsDTO
+}
+
+func SingleSubjectToDTO(subject *services.Subject) *Subject {
+	return &Subject{
+		Name: subject.Name,
+		Slug: subject.Slug,
+		ID:   subject.ID,
+	}
+
+}
+
+func SubjectsToDTO(subjects []services.Subject) []Subject {
+	SubjectsDTO := []Subject{}
+	for _, subject := range subjects {
+		SubjectsDTO = append(SubjectsDTO, *SingleSubjectToDTO(&subject))
+	}
+	return SubjectsDTO
+}
+
+//Handler functions:
+
+//
+func handleSubjectsGet(w http.ResponseWriter, r *http.Request) {
+	serviceSubjects, err := services.GetSubjects(nil)
+	if err != nil {
+		restError(w, r, err, http.StatusBadRequest)
+		return
+	}
+	outSubjects := SubjectsToDTO(serviceSubjects)
+	if err = json.NewEncoder(w).Encode(outSubjects); err != nil {
+		restError(w, r, err, http.StatusInternalServerError)
+		return
+	}
+
+}
+
+//checks for a filter and if none returns all tutors and if a filter returns tutors for that subject
+func handleSubjectTutorsGet(w http.ResponseWriter, r *http.Request) {
+	q := r.URL.Query()
+	filter := q.Get("filter")
+	services.CreateSubjectTestAccounts()
+	if filter != "" {
+		filtered, err := services.GetSubjectByName(filter, nil)
+		if err != nil {
+			restError(w, r, err, http.StatusBadRequest)
+			return
+		}
+		services.CreateSubjectTestAccounts()
+		tutors, err := services.GetTutorsBySubjectID(filtered.ID, nil)
+		if err != nil {
+			restError(w, r, err, http.StatusBadRequest)
+			return
+		}
+
+		outTutors := TutorSubjectsToDTO(tutors)
+
+		if err = json.NewEncoder(w).Encode(outTutors); err != nil {
+			restError(w, r, err, http.StatusInternalServerError)
+			return
+		}
+
+	} else {
+		tutors, err := services.GetAllTutors(nil)
+		if err != nil {
+			restError(w, r, err, http.StatusBadRequest)
+			return
+		}
+		outTutors := TutorSubjectsToDTO(tutors)
+		if err = json.NewEncoder(w).Encode(outTutors); err != nil {
+			restError(w, r, err, http.StatusInternalServerError)
+			return
+		}
+	}
 
 }
--- api/pkg/services/subjects.go
+++ api/pkg/services/subjects.go
@@ -1,21 +1,247 @@
 package services
 
-import "github.com/cs3305-team-4/api/pkg/database"
+import (
+	"fmt"
 
+	"github.com/cs3305-team-4/api/pkg/database"
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+//Subject contains information about a single subject
 type Subject struct {
 	database.Model
-	Name string
+	Name string `gorm:"unique;not null;"`
 	Slug string
 }
 
+type SubjectTaughtError string
+
+func (e SubjectTaughtError) Error() string {
+	return string(e)
+}
+
+const (
+	SubjectTaughtErrorDoesNotExist SubjectTaughtError = "This tutor subject relation does not exist"
+)
+
+//Contains information on
 type SubjectTaught struct {
 	database.Model
-	Subject     Subject
-	Tutor       Account
-	Price       uint
+
+	//Contains Subject bring taught
+	Subject   Subject `gorm:"foreignKey:SubjectID"`
+	SubjectID uuid.UUID
+
+	//
+	Tutor   Account `gorm:"foreignKey:TutorID"`
+	TutorID uuid.UUID
+
+	//Price that the Tutor wishes to charge per lesson
+	Price uint
+	//Description given by the tutor
 	Description string
 }
 
-func ReadSubjects() ([]Subject, error) {
-	return nil, nil
+//gets all subjects in the DB
+func GetSubjects(db *gorm.DB) ([]Subject, error) {
+	if db == nil {
+		var err error
+		db, err = database.Open()
+		if err != nil {
+			return nil, err
+		}
+	}
+	var subject []Subject
+	return subject, db.Find(&subject).Error
+}
+
+// returns a subject when given a subject name
+func GetSubjectByName(name string, db *gorm.DB) (*Subject, error) {
+	if db == nil {
+		var err error
+		db, err = database.Open()
+		if err != nil {
+			return nil, err
+		}
+	}
+	subject := &Subject{}
+	return subject, db.Where("Name = ?", name).Find(&subject).Error
+}
+
+//Quries the DB for SubjectTaught where the subject ID is a match
+func GetTutorsBySubjectID(sid uuid.UUID, db *gorm.DB) ([]SubjectTaught, error) {
+	if db == nil {
+		var err error
+		db, err = database.Open()
+		if err != nil {
+			return nil, err
+		}
+	}
+	var subjectTaught []SubjectTaught
+	return subjectTaught, db.Where(&SubjectTaught{Subject: Subject{Model: database.Model{ID: sid}}}).Find(&subjectTaught).Error
+}
+
+//Quries the DB for SubjectTaught where the ID matches the SubjectTaught ID
+func GetSubjectTaughtByID(stid uuid.UUID, db *gorm.DB) (*SubjectTaught, error) {
+	if db == nil {
+		var err error
+		db, err = database.Open()
+		if err != nil {
+			return nil, err
+		}
+	}
+	subjectTaught := &SubjectTaught{}
+	return subjectTaught, db.Where(&SubjectTaught{Model: database.Model{ID: stid}}).Find(&subjectTaught).Error
+}
+
+//Returns all subjectTaught
+func GetAllTutors(db *gorm.DB) ([]SubjectTaught, error) {
+	if db == nil {
+		var err error
+		db, err = database.Open()
+		if err != nil {
+			return nil, err
+		}
+	}
+	var subjectTaught []SubjectTaught
+	return subjectTaught, db.Find(&subjectTaught).Error
+}
+
+//Returns subjectTaught for specific Tutors
+func GetSubjectsByTutorID(tid uuid.UUID, db *gorm.DB) ([]SubjectTaught, error) {
+	if db == nil {
+		var err error
+		db, err = database.Open()
+		if err != nil {
+			return nil, err
+		}
+	}
+	var subjectTaught []SubjectTaught
+	return subjectTaught, db.Where(&SubjectTaught{Tutor: Account{Model: database.Model{ID: tid}}}).Find(&subjectTaught).Error
+}
+
+func teachSubject(subject *Subject, tutor *Account, description string, price uint, db *gorm.DB) error {
+	db, err := database.Open()
+	if err != nil {
+		return err
+	}
+	if !tutor.IsTutor() {
+		return fmt.Errorf("account specified is not a tutor account and thus cannot teach")
+	}
+	if subject == nil {
+		return fmt.Errorf("there must be a subject to teach")
+	}
+
+	return db.Create(&SubjectTaught{Subject: *subject, Tutor: *tutor, Price: price, Description: description}).Error
+
+}
+
+func updateCost(stid uuid.UUID, price uint, db *gorm.DB) (*SubjectTaught, error) {
+	db, err := database.Open()
+	if err != nil {
+		return nil, err
+	}
+	var subjectTaught *SubjectTaught
+	return subjectTaught, db.Transaction(func(tx *gorm.DB) error {
+		dbSubjectTaught, err := GetSubjectTaughtByID(stid, nil)
+		if err != nil {
+			return err
+		}
+		if subjectTaught = dbSubjectTaught; subjectTaught == nil {
+			return SubjectTaughtErrorDoesNotExist
+		}
+		return tx.Model(subjectTaught).Update("Price", price).Error
+	})
+}
+
+func updateDescription(stid uuid.UUID, description string, db *gorm.DB) (*SubjectTaught, error) {
+	db, err := database.Open()
+	if err != nil {
+		return nil, err
+	}
+	var subjectTaught *SubjectTaught
+	return subjectTaught, db.Transaction(func(tx *gorm.DB) error {
+		dbSubjectTaught, err := GetSubjectTaughtByID(stid, nil)
+		if err != nil {
+			return err
+		}
+		if subjectTaught = dbSubjectTaught; subjectTaught == nil {
+			return SubjectTaughtErrorDoesNotExist
+		}
+		return tx.Model(subjectTaught).Update("Description", description).Error
+	})
+}
+
+func CreateSubjectTestAccounts() error {
+	db, err := database.Open()
+	db.Create(&Subject{Name: "English"})
+	db.Create(&Subject{Name: "Maths"})
+	if err != nil {
+		return err
+	}
+
+	hash, err := NewPasswordHash("grindshub")
+	if err != nil {
+		return err
+	}
+
+	db.FirstOrCreate(&Account{
+		Model: database.Model{
+			ID: uuid.MustParse("deadbeef-cafe-badd-c0de-facadebadbad"),
+		},
+		Email:         "tutor@grindshub.localhost",
+		EmailVerified: true,
+		Type:          Tutor,
+		Suspended:     false,
+		PasswordHash:  *hash,
+		Profile: &Profile{
+			Avatar:         "",
+			Slug:           "Mike-tutor",
+			FirstName:      "Mike",
+			LastName:       "Tutor",
+			City:           "Cork",
+			Country:        "Ireland",
+			Description:    "A tutor",
+			Qualifications: []Qualification{},
+			WorkExperience: []WorkExperience{},
+		},
+	})
+	if err != nil {
+		return err
+	}
+
+	db.FirstOrCreate(&Account{
+		Model: database.Model{
+			ID: uuid.MustParse("deadbeef-cafe-badd-c0de-facadebadbad"),
+		},
+		Email:         "tutor2@grindshub.localhost",
+		EmailVerified: true,
+		Type:          Tutor,
+		Suspended:     false,
+		PasswordHash:  *hash,
+		Profile: &Profile{
+			Avatar:         "",
+			Slug:           "john-tutor",
+			FirstName:      "John",
+			LastName:       "Tutor",
+			City:           "Cork",
+			Country:        "Ireland",
+			Description:    "A tutor",
+			Qualifications: []Qualification{},
+			WorkExperience: []WorkExperience{},
+		},
+	})
+	if err != nil {
+		return err
+	}
+
+	John, err := ReadAccountByEmail("tutor@grindshub.localhost2", nil)
+	Mike, err := ReadAccountByEmail("tutor@grindshub.localhost", nil)
+	English, err := GetSubjectByName("English", nil)
+	Maths, err := GetSubjectByName("Maths", nil)
+	teachSubject(Maths, John, "John teaches stuff", 70, nil)
+	teachSubject(English, Mike, "Mike teaches better stuff", 75, nil)
+
+	return nil
 }
