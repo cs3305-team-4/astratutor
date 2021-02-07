ed10036a1411bd8ee13d05b14e08a32a1b476d41
ocanty <git@ocanty.com>
2021-02-07 06:05:43 +0000

    made landing use grid rather than text-align

--- ui/src/views/Landing.tsx
+++ ui/src/views/Landing.tsx
@@ -4,20 +4,15 @@ import styled from 'styled-components';
 
 import {
   Typography,
-  Layout
+  Layout,
+  Row,
+  Col
 } from "antd";
 
-const { Title, Paragraph, Text, Link } = Typography;
-const { Header, Footer, Sider, Content } = Layout;
+const { Title, Paragraph, Text, Link } = Typography
+const { Header, Footer, Sider, Content } = Layout
 
 const Hero = styled(Content)`
-  padding: 25vh 0;
-  text-align: center;
-
-  h1 {
-    font-size: 4rem;
-  }
-
   background-color: rgba(233,233,233);
 `
 
@@ -25,11 +20,19 @@ export default function Landing() {
   return (
     <Layout>
       <Hero>
-        <Typography>
-          <h1>
-            Give your grades a blast off!
-          </h1>
-        </Typography>
+        <Row 
+          style={{height: "calc(100vh - 72px)"}}
+          align="middle"
+          justify="center"
+        >
+          <Col>
+            <Typography>
+              <Title level={1}>
+                Take your marks to the stars!
+              </Title>
+            </Typography>
+          </Col>
+        </Row>
       </Hero>
     </Layout>
   )
