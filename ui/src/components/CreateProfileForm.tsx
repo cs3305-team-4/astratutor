2cfb1d183a02f45065cec801e99a39b2d1b575f7
ocanty <git@ocanty.com>
2021-02-07 23:35:34 +0000

    linted ui

--- ui/src/components/CreateProfileForm.tsx
+++ ui/src/components/CreateProfileForm.tsx
@@ -23,94 +23,85 @@ const { Header, Footer, Sider, Content } = Layout;
 const { TextArea } = Input;
 
 export interface ProfileProps {
-    uuid: string;
-    type: AccountType;
+  uuid: string;
+  type: AccountType;
 }
 
 export default function CreateProfileForm(props: ProfileProps) {
-    const [error, setError] = React.useState<string>('');
-    const auth = React.useContext(AuthContext);
-    const history = useHistory();
+  const [error, setError] = React.useState<string>('');
+  const auth = React.useContext(AuthContext);
+  const history = useHistory();
 
-    const [createProfile, setCreateProfile] = React.useState<CreateProfileDTO>({
-        first_name: '',
-        last_name: '',
-        avatar: '',
-        city: '',
-        country: '',
-    });
+  const [createProfile, setCreateProfile] = React.useState<CreateProfileDTO>({
+    first_name: '',
+    last_name: '',
+    avatar: '',
+    city: '',
+    country: '',
+  });
 
-    const onSubmitCreateProfile = async (values: CreateProfileDTO) => {
-        try {
-            const res = await fetchRest(`${config.apiUrl}/${props.type}s/${props.uuid}/profile`, {
-                method: 'POST',
-                body: JSON.stringify(values),
-                headers: {
-                    Authorization: `Bearer ${auth.bearerToken}`,
-                },
-            });
+  const onSubmitCreateProfile = async (values: CreateProfileDTO) => {
+    try {
+      const res = await fetchRest(`${config.apiUrl}/${props.type}s/${props.uuid}/profile`, {
+        method: 'POST',
+        body: JSON.stringify(values),
+        headers: {
+          Authorization: `Bearer ${auth.bearerToken}`,
+        },
+      });
 
-            history.push('/account');
-        } catch (e) {
-            setError(`could not create profile: ${e}`);
-        }
-    };
+      history.push('/account');
+    } catch (e) {
+      setError(`could not create profile: ${e}`);
+    }
+  };
 
-    return (
-        <PageHeader
-            title={
-                <>
-                    <Title level={3}>
-                        <UserAddOutlined /> Create Profile
-                    </Title>
-                </>
-            }
-            className="site-page-header"
-        >
-            <Content>
-                <Row gutter={4}>
-                    <Col md={12} sm={24} xs={24}>
-                        <Form layout="vertical" name="create-profile" onFinish={onSubmitCreateProfile}>
-                            <UserAddOutlined
-                                style={{
-                                    display: 'block',
-                                    margin: '0 auto',
-                                    fontSize: '4rem',
-                                    padding: '2rem',
-                                    color: 'rgb(200,200,200)',
-                                }}
-                            />
-                            <Form.Item
-                                name="first_name"
-                                rules={[{ required: true, message: 'Please input your first name!' }]}
-                            >
-                                <Input placeholder="First Name" />
-                            </Form.Item>
-                            <Form.Item
-                                name="last_name"
-                                rules={[{ required: true, message: 'Please input your last name!' }]}
-                            >
-                                <Input placeholder="Last Name" />
-                            </Form.Item>
-                            <Form.Item name="city" rules={[{ required: true, message: 'Please input your city!' }]}>
-                                <Input placeholder="City" />
-                            </Form.Item>
-                            <Form.Item
-                                name="country"
-                                rules={[{ required: true, message: 'Please input your country!' }]}
-                            >
-                                <Input placeholder="Country" />
-                            </Form.Item>
-                            <Form.Item>
-                                <Button style={{ width: '100%' }} type="primary" htmlType="submit">
-                                    Create Profile
-                                </Button>
-                            </Form.Item>
-                        </Form>
-                        <span style={{ color: 'red' }}>{error}</span>
-                    </Col>
-                </Row>
-            </Content>
-        </PageHeader>
-    );
+  return (
+    <PageHeader
+      title={
+        <>
+          <Title level={3}>
+            <UserAddOutlined /> Create Profile
+          </Title>
+        </>
+      }
+      className="site-page-header"
+    >
+      <Content>
+        <Row gutter={4}>
+          <Col md={12} sm={24} xs={24}>
+            <Form layout="vertical" name="create-profile" onFinish={onSubmitCreateProfile}>
+              <UserAddOutlined
+                style={{
+                  display: 'block',
+                  margin: '0 auto',
+                  fontSize: '4rem',
+                  padding: '2rem',
+                  color: 'rgb(200,200,200)',
+                }}
+              />
+              <Form.Item name="first_name" rules={[{ required: true, message: 'Please input your first name!' }]}>
+                <Input placeholder="First Name" />
+              </Form.Item>
+              <Form.Item name="last_name" rules={[{ required: true, message: 'Please input your last name!' }]}>
+                <Input placeholder="Last Name" />
+              </Form.Item>
+              <Form.Item name="city" rules={[{ required: true, message: 'Please input your city!' }]}>
+                <Input placeholder="City" />
+              </Form.Item>
+              <Form.Item name="country" rules={[{ required: true, message: 'Please input your country!' }]}>
+                <Input placeholder="Country" />
+              </Form.Item>
+              <Form.Item>
+                <Button style={{ width: '100%' }} type="primary" htmlType="submit">
+                  Create Profile
+                </Button>
+              </Form.Item>
+            </Form>
+            <span style={{ color: 'red' }}>{error}</span>
+          </Col>
+        </Row>
+      </Content>
+    </PageHeader>
+  );
 }
--- ui/src/views/Account.tsx
+++ ui/src/views/Account.tsx
@@ -6,12 +6,12 @@ import { Link, useHistory, Switch, Route, useRouteMatch, useLocation } from 'rea
 import { Typography, Layout, Menu, Row, Col } from 'antd';
 
 import {
-    LockOutlined,
-    MailOutlined,
-    UserOutlined,
-    BankOutlined,
-    AppstoreOutlined,
-    SettingOutlined,
+  LockOutlined,
+  MailOutlined,
+  UserOutlined,
+  BankOutlined,
+  AppstoreOutlined,
+  SettingOutlined,
 } from '@ant-design/icons';
 
 import AccountProfile from '../components/AccountProfile';
@@ -23,89 +23,66 @@ const { Title, Paragraph, Text } = Typography;
 const { Header, Footer, Sider, Content } = Layout;
 
 const StyledLayout = styled(Layout)`
-    background-color: rgba(233, 233, 233);
+  background-color: rgba(233, 233, 233);
 `;
 
 const StyledSider = styled(Sider)`
-    background-color: rgba(233, 233, 233);
+  background-color: rgba(233, 233, 233);
 `;
 
 export default function Account() {
-    const history = useHistory();
-    const { path, url } = useRouteMatch();
+  const history = useHistory();
+  const { path, url } = useRouteMatch();
 
-    const location = useLocation();
+  const location = useLocation();
 
-    return (
-        <StyledLayout>
-            <StyledSider>
-                <Menu selectedKeys={[location.pathname.substr(path.length)]} mode="inline">
-                    <Menu.Item onClick={() => history.push(`${path}/profile`)} key="/profile" icon={<UserOutlined />}>
-                        Profile
-                    </Menu.Item>
-                    <Menu.Item onClick={() => history.push(`${path}/billing`)} key="/billing" icon={<BankOutlined />}>
-                        Billing
-                    </Menu.Item>
-                </Menu>
-            </StyledSider>
-            <Content style={{ minHeight: 'calc(100vh - 72px)' }}>
-                <Switch>
-                    <Route exact path={`${path}/profile/create`}>
-                        <Row>
-                            <Col md={9} sm={6} xs={0} />
-                            <Col
-                                md={24}
-                                sm={24}
-                                xs={24}
-                                style={{ padding: '0.5rem', backgroundColor: 'rgba(255,255,255,0.8)' }}
-                            >
-                                <AuthContext.Consumer>
-                                    {(auth) =>
-                                        auth.isLoggedIn() ? (
-                                            <CreateProfileForm uuid={auth.claims.sub} type={auth.account.type} />
-                                        ) : (
-                                            <></>
-                                        )
-                                    }
-                                </AuthContext.Consumer>
-                            </Col>
-                            <Col md={9} sm={6} xs={0} />
-                        </Row>
-                    </Route>
-                    <Route exact path={`${path}/profile`}>
-                        <Row>
-                            <Col
-                                md={24}
-                                sm={24}
-                                xs={24}
-                                style={{ padding: '0.5rem', backgroundColor: 'rgba(255,255,255,0.8)' }}
-                            >
-                                <AuthContext.Consumer>
-                                    {(auth) =>
-                                        auth.isLoggedIn() ? (
-                                            <AccountProfile uuid={auth.claims.sub} type={auth.account.type} />
-                                        ) : (
-                                            <></>
-                                        )
-                                    }
-                                </AuthContext.Consumer>
-                            </Col>
-                        </Row>
-                    </Route>
-                    <Route exact path={`${path}/billing`}>
-                        <Row>
-                            <Col
-                                md={24}
-                                sm={10}
-                                xs={24}
-                                style={{ padding: '1rem', backgroundColor: 'rgba(255,255,255,0.8)' }}
-                            >
-                                Billing
-                            </Col>
-                        </Row>
-                    </Route>
-                </Switch>
-            </Content>
-        </StyledLayout>
-    );
+  return (
+    <StyledLayout>
+      <StyledSider>
+        <Menu selectedKeys={[location.pathname.substr(path.length)]} mode="inline">
+          <Menu.Item onClick={() => history.push(`${path}/profile`)} key="/profile" icon={<UserOutlined />}>
+            Profile
+          </Menu.Item>
+          <Menu.Item onClick={() => history.push(`${path}/billing`)} key="/billing" icon={<BankOutlined />}>
+            Billing
+          </Menu.Item>
+        </Menu>
+      </StyledSider>
+      <Content style={{ minHeight: 'calc(100vh - 72px)' }}>
+        <Switch>
+          <Route exact path={`${path}/profile/create`}>
+            <Row>
+              <Col md={9} sm={6} xs={0} />
+              <Col md={24} sm={24} xs={24} style={{ padding: '0.5rem', backgroundColor: 'rgba(255,255,255,0.8)' }}>
+                <AuthContext.Consumer>
+                  {(auth) =>
+                    auth.isLoggedIn() ? <CreateProfileForm uuid={auth.claims.sub} type={auth.account.type} /> : <></>
+                  }
+                </AuthContext.Consumer>
+              </Col>
+              <Col md={9} sm={6} xs={0} />
+            </Row>
+          </Route>
+          <Route exact path={`${path}/profile`}>
+            <Row>
+              <Col md={24} sm={24} xs={24} style={{ padding: '0.5rem', backgroundColor: 'rgba(255,255,255,0.8)' }}>
+                <AuthContext.Consumer>
+                  {(auth) =>
+                    auth.isLoggedIn() ? <AccountProfile uuid={auth.claims.sub} type={auth.account.type} /> : <></>
+                  }
+                </AuthContext.Consumer>
+              </Col>
+            </Row>
+          </Route>
+          <Route exact path={`${path}/billing`}>
+            <Row>
+              <Col md={24} sm={10} xs={24} style={{ padding: '1rem', backgroundColor: 'rgba(255,255,255,0.8)' }}>
+                Billing
+              </Col>
+            </Row>
+          </Route>
+        </Switch>
+      </Content>
+    </StyledLayout>
+  );
 }
