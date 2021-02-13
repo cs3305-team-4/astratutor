96ff192e73346be2c8a1d0209363d954bfb88d2a
ocanty <git@ocanty.com>
2021-02-13 20:51:03 +0000

    implemented lessons view

---
+++ ui/src/views/Lessons.tsx
@@ -0,0 +1,143 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { Layout, Menu } from 'antd';
+
+import { MailOutlined, ScheduleOutlined, FileDoneOutlined } from '@ant-design/icons';
+
+import { APIContext } from '../api/api';
+import { AccountType, LessonRequestStage, LessonResponseDTO, ProfileResponseDTO } from '../api/definitions';
+import { useAsync } from 'react-async-hook';
+import Lesson, { LessonProps } from '../components/Lesson';
+
+const { Sider, Content } = Layout;
+
+enum Menus {
+  Requests = 'Pending Requests',
+  Scheduled = 'Scheduled',
+  Completed = 'Completed',
+  Cancelled = 'Cancelled',
+  Denied = 'Denied',
+}
+
+const StyledLayout = styled(Layout)`
+  background-color: white;
+`;
+
+const StyledSider = styled(Sider)`
+  background-color: rgba(233, 233, 233);
+`;
+
+export function Lessons(): React.ReactElement {
+  const api = React.useContext(APIContext);
+  const [menu, setMenu] = React.useState<Menus>(Menus.Requests);
+
+  const [lessonProps, setLessonProps] = React.useState<{ [uuid: string]: LessonProps }>({});
+
+  useAsync(async () => {
+    const lessons: LessonResponseDTO[] = await api.services.readLessonsByAccountId(api.account.id);
+
+    const profiles: { [uuid: string]: ProfileResponseDTO } = {};
+
+    const lprops: { [uuid: string]: LessonProps } = {};
+
+    // go through the lessons and request any of the profiles we need to display each lesson
+    for (const lesson of lessons) {
+      if (!(lesson.student_id in profiles) && lesson.student_id !== api.account.id) {
+        profiles[lesson.student_id] = await api.services.readProfileByAccountID(lesson.student_id, AccountType.Student);
+      }
+
+      if (!(lesson.tutor_id in profiles) && lesson.tutor_id !== api.account.id) {
+        profiles[lesson.tutor_id] = await api.services.readProfileByAccountID(lesson.tutor_id, AccountType.Tutor);
+      }
+
+      lprops[lesson.id] = {
+        lesson,
+        otherProfile: lesson.student_id !== api.account.id ? profiles[lesson.student_id] : profiles[lesson.tutor_id],
+        onUpdate: (lnew: LessonResponseDTO, otherProfile: ProfileResponseDTO) => {
+          setLessonProps((lprops: { [uuid: string]: LessonProps }) => {
+            const lpropsnew = { ...lprops };
+            lpropsnew[lnew.id].lesson = lnew;
+            lpropsnew[lnew.id].otherProfile = otherProfile;
+            return lpropsnew;
+          });
+        },
+      };
+    }
+
+    setLessonProps(lprops);
+  }, []);
+
+  return (
+    <StyledLayout>
+      <StyledSider>
+        <Menu selectedKeys={[menu]} mode="inline">
+          <Menu.Item onClick={() => setMenu(Menus.Requests)} key={Menus.Requests} icon={<MailOutlined />}>
+            Requests
+          </Menu.Item>
+          <Menu.Item onClick={() => setMenu(Menus.Scheduled)} key={Menus.Scheduled} icon={<ScheduleOutlined />}>
+            Scheduled
+          </Menu.Item>
+          <Menu.Item onClick={() => setMenu(Menus.Completed)} key={Menus.Completed} icon={<FileDoneOutlined />}>
+            Completed
+          </Menu.Item>
+          <Menu.Item onClick={() => setMenu(Menus.Cancelled)} key={Menus.Cancelled} icon={<FileDoneOutlined />}>
+            Cancelled
+          </Menu.Item>
+          <Menu.Item onClick={() => setMenu(Menus.Denied)} key={Menus.Denied} icon={<FileDoneOutlined />}>
+            Denied
+          </Menu.Item>
+        </Menu>
+      </StyledSider>
+      <Content style={{ minHeight: 'calc(100vh - 72px)' }}>
+        {menu === Menus.Requests &&
+          Object.keys(lessonProps).map((key: string, index: number) => {
+            if (lessonProps[key].lesson.request_stage === LessonRequestStage.Requested) {
+              return <Lesson key={index} {...lessonProps[key]} />;
+            }
+
+            return <></>;
+          })}
+        {menu === Menus.Scheduled &&
+          Object.keys(lessonProps).map((key: string, index: number) => {
+            if (lessonProps[key].lesson.request_stage === LessonRequestStage.Accepted) {
+              return <Lesson key={index} {...lessonProps[key]} />;
+            }
+
+            return <></>;
+          })}
+        {menu === Menus.Completed &&
+          Object.keys(lessonProps).map((key: string, index: number) => {
+            if (lessonProps[key].lesson.request_stage === LessonRequestStage.Completed) {
+              return <Lesson key={index} {...lessonProps[key]} />;
+            }
+
+            return <></>;
+          })}
+        {menu === Menus.Cancelled &&
+          Object.keys(lessonProps).map((key: string, index: number) => {
+            if (lessonProps[key].lesson.request_stage === LessonRequestStage.Cancelled) {
+              return <Lesson key={index} {...lessonProps[key]} />;
+            }
+
+            return <></>;
+          })}
+        {menu === Menus.Denied &&
+          Object.keys(lessonProps).map((key: string, index: number) => {
+            if (lessonProps[key].lesson.request_stage === LessonRequestStage.Denied) {
+              return <Lesson key={index} {...lessonProps[key]} />;
+            }
+
+            return <></>;
+          })}
+        {/* {menu === Menus.Scheduled && (
+          
+        )}
+        ap((lessonProp: LessonProps, index: number) => <Lesson key={index} {...lessonProp} />)}
+        {menu === Menus.Completed && (
+          
+        )} */}
+      </Content>
+    </StyledLayout>
+  );
+}
