4dd9ac822564e26c89fe44aed367e20ce32fdc8f
ocanty <git@ocanty.com>
2021-02-13 20:49:38 +0000

    auth -> api

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,36 +1,34 @@
-import React, { useContext } from 'react';
+import React from 'react';
 
 import 'antd/dist/antd.css';
-import { Layout, PageHeader, Button, Divider, Row, Col, Typography } from 'antd';
+import { Layout, PageHeader, Button, Divider, Row, Col } from 'antd';
 
 import { UserOutlined } from '@ant-design/icons';
 
 import { Switch, Route, Link, useHistory, useLocation } from 'react-router-dom';
 
-import Account from './views/Account';
-import Landing from './views/Landing';
-import Login from './views/Login';
-import Register from './views/Register';
+import { Account } from './views/Account';
+import { Landing } from './views/Landing';
+import { Login } from './views/Login';
+import { Lessons } from './views/Lessons';
+import { ViewProfile } from './views/ViewProfile';
+import { Register } from './views/Register';
 import './App.css';
 
-import config from './config';
-import { fetchRest } from './api/rest';
-import { AuthContext, useAuthValues, PrivateRoute } from './api/auth';
-import { AuthClaims } from './api/auth';
+import { APIContext, useApiValues, PrivateRoute } from './api/api';
+
 import { useAsync } from 'react-async-hook';
 
-const { Header, Footer, Sider, Content } = Layout;
-const { Title, Paragraph, Text } = Typography;
+const { Footer, Content } = Layout;
 
-function App() {
+function App(): React.ReactElement {
   const history = useHistory();
-  const auth = useAuthValues();
+  const api = useApiValues();
   const location = useLocation();
 
-  React.useEffect(() => {
+  useAsync(async () => {
     try {
-      console.log('hi');
-      auth.loginSilent();
+      await api.loginSilent();
     } catch (e) {
       console.error(`error attempting to login from localStorage ${e}`);
     }
@@ -38,35 +36,21 @@ function App() {
 
   // If login state has changed, check if their profile exists
   useAsync(async () => {
-    if (auth.isLoggedIn()) {
+    if (api.isLoggedIn()) {
       // Check if the user has a profile
-      try {
-        const res = await fetchRest(
-          `${config.apiUrl}/${auth.account.type}s/${auth.claims.sub}/profile`,
-          {
-            headers: {
-              Authorization: `Bearer ${auth.bearerToken}`,
-            },
-          },
-          [200, 404],
-        );
-
-        // No profile, redirect   them
-        if (res.status === 404 && location.pathname !== '/account/profile/create') {
+      if (!(await api.services.accountHasProfile(api.account.id, api.account.type))) {
+        if (location.pathname !== '/account/profile/create') {
           history.replace('/account/profile/create');
         }
-      } catch (e) {
-        console.log(e);
-        // TODO(ocanty) - errorhandling
       }
     }
-  }, [auth, location.pathname]);
+  }, [api, location.pathname]);
 
   // Don't render the page until the silent login attempt is finished
-  if (!auth.loginSilentFinished()) return <AuthContext.Provider value={auth}></AuthContext.Provider>;
+  if (!api.loginSilentFinished()) return <APIContext.Provider value={api}></APIContext.Provider>;
 
   let headerLinks = [];
-  if (auth.isLoggedIn()) {
+  if (api.isLoggedIn()) {
     headerLinks = [
       <Link to="/" key="home">
         <Button type="text">Home</Button>
@@ -86,7 +70,7 @@ function App() {
           Account
         </Button>
       </Link>,
-      <Button key="logout" onClick={() => auth.logout()}>
+      <Button key="logout" onClick={() => api.logout()}>
         Logout
       </Button>,
     ];
@@ -111,48 +95,47 @@ function App() {
   }
 
   return (
-    <AuthContext.Provider value={auth}>
-      <Layout style={{ minHeight: '100vh' }}>
-        <PageHeader
-          ghost={false}
-          title={
-            <Link to="/" key="logo-home">
-              <span>AstraTutor</span>
-            </Link>
-          }
-          extra={headerLinks}
-        />
-        <Content>
-          <Switch>
-            <Route path="/" exact={true}>
-              <Landing />
-            </Route>
-            <PrivateRoute path="/account" component={Account} />
-
-            <Route path="/subjects"></Route>
-            <Route path="/subjects/:subject_slug/tutors"></Route>
-            <Route path="/tutors/:slug"></Route>
-            <Route path="/tutors/:slug/profile"></Route>
-            <PrivateRoute path="/lessons" />
-            <PrivateRoute path="/lessons/:lid" />
-            <PrivateRoute path="/lessons/:lid/lobby" />
-            <PrivateRoute path="/lessons/:lid/classroom" />
-            <Route path="/login" component={Login} />
-            <Route path="/register" component={Register} />
-          </Switch>
-        </Content>
-        <Footer>
-          <Divider orientation="left">AstraTutor</Divider>
-          <Row>
-            <Col flex={16}>Site Map</Col>
-            <Col flex={24 - 16}>Links</Col>
-          </Row>
-          <Row style={{ margin: '0 auto', textAlign: 'center' }}>
-            <p>Made with love by CS3505 Team 4</p>
-          </Row>
-        </Footer>
-      </Layout>
-    </AuthContext.Provider>
+    <APIContext.Provider value={api}>
+      <APIContext.Provider value={api}>
+        <Layout style={{ minHeight: '100vh' }}>
+          <PageHeader
+            ghost={false}
+            title={
+              <Link to="/" key="logo-home">
+                <span>AstraTutor</span>
+              </Link>
+            }
+            extra={headerLinks}
+            style={{ boxShadow: '0 1px 10px rgba(0,0,0,0.25)' }}
+          />
+          <Content>
+            <Switch>
+              <Route path="/" exact={true}>
+                <Landing />
+              </Route>
+              <PrivateRoute path="/account" component={Account} />
+              <Route path="/subjects"></Route>
+              <Route path="/subjects/:subject_slug/tutors"></Route>
+              <Route exact path="/tutors/:slug"></Route>
+              <Route exact path="/tutors/:uuid/profile" component={ViewProfile} />
+              <PrivateRoute path="/lessons" component={Lessons} />
+              <Route path="/login" component={Login} />
+              <Route path="/register" component={Register} />
+            </Switch>
+          </Content>
+          <Footer>
+            <Divider orientation="left">AstraTutor</Divider>
+            <Row>
+              <Col flex={16}>Site Map</Col>
+              <Col flex={24 - 16}>Links</Col>
+            </Row>
+            <Row style={{ margin: '0 auto', textAlign: 'center' }}>
+              <p>Made with love by CS3505 Team 4</p>
+            </Row>
+          </Footer>
+        </Layout>
+      </APIContext.Provider>
+    </APIContext.Provider>
   );
 }
 
