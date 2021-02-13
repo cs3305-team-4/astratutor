a911b9cb3d08e8ad6ad0ebcb62fc0893c4c881a1
ocanty <git@ocanty.com>
2021-02-13 20:50:43 +0000

    api models for services

--- ui/src/api/definitions.ts
+++ ui/src/api/definitions.ts
@@ -3,11 +3,35 @@ export interface LoginDTO {
   password: string;
 }
 
+export enum AccountType {
+  Student = 'student',
+  Tutor = 'tutor',
+}
+
+export interface AccountRequestDTO {
+  email: string;
+  password: string;
+  type: AccountType;
+  parents_email?: string;
+}
+
+export interface LoginRequestDTO {
+  email: string;
+  password: string;
+}
+
 export interface LoginResponseDTO {
   jwt: string;
 }
 
-export interface ReadProfileDTO {
+export interface AccountResponseDTO {
+  id: string;
+  email: string;
+  type: AccountType;
+  parents_email?: string;
+}
+export interface ProfileResponseDTO {
+  account_id: string;
   avatar: string;
   slug: string;
   first_name: string;
@@ -16,12 +40,12 @@ export interface ReadProfileDTO {
   country: string;
   subtitle: string;
   description: string;
-  qualifications?: QualificationDTO[];
-  work_experience?: WorkExperienceDTO[];
+  qualifications?: QualificationResponseDTO[];
+  work_experience?: WorkExperienceResponseDTO[];
   availability?: boolean[];
 }
 
-export interface CreateProfileDTO {
+export interface ProfileRequestDTO {
   avatar: string;
   first_name: string;
   last_name: string;
@@ -29,26 +53,65 @@ export interface CreateProfileDTO {
   country: string;
 }
 
-export interface QualificationDTO {
+export interface QualificationRequestDTO {
+  field: string;
+  degree: string;
+  school: string;
+}
+
+export interface QualificationResponseDTO {
+  id: string;
   field: string;
   degree: string;
   school: string;
   verified: boolean;
 }
 
-export interface WorkExperienceDTO {
+export interface WorkExperienceRequestDTO {
+  role: string;
+  years_exp: string;
+  description: string;
+}
+
+export interface WorkExperienceResponseDTO {
+  id: string;
   role: string;
   years_exp: string;
   description: string;
   verified: boolean;
 }
 
-export enum AccountType {
-  Student = 'student',
-  Tutor = 'tutor',
+export interface LessonRequestDTO {
+  start_time: string; // RFC3339 timestamp
+  tutor_id: string;
+  student_id: string;
+  lesson_detail: string;
 }
-export interface AccountDTO {
-  email: string;
-  type: AccountType;
-  parents_email?: string;
+
+export enum LessonRequestStage {
+  Requested = 'requested',
+  Accepted = 'accepted',
+  Denied = 'denied',
+  Cancelled = 'cancelled',
+  Completed = 'completed',
+  NoShowStudent = 'no-show-student',
+  NoShowTutor = 'no-show-tutor',
+  Expired = 'expired',
+}
+export interface LessonResponseDTO {
+  id: string;
+  start_time: string; // RFC3339 timestap
+
+  requester_id: string;
+  student_id: string;
+  tutor_id: string;
+  lesson_detail: string;
+  request_stage: LessonRequestStage;
+  request_stage_detail: string;
+  request_stage_changer_id: string;
+}
+
+// i.e confirmed, expired, etc
+export interface LessonStageChangeDTO {
+  stage_detail: string;
 }
