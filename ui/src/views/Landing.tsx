34b34258cfd4d0ffc3b6e7a0fcbd3e61f6f27782
Eric Moynihan <git@moynihan.io>
2021-03-07 17:30:37 +0000

    Added gradient v1

--- ui/src/views/Landing.tsx
+++ ui/src/views/Landing.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import styled from 'styled-components';
 
 import { Typography, Layout, Row, Col } from 'antd';
@@ -11,11 +11,56 @@ const Hero = styled(Content)`
 `;
 
 export function Landing(): React.ReactElement {
+  const canvasRef = useRef<HTMLCanvasElement>();
+
+  useEffect(() => {
+    const context = canvasRef.current?.getContext('2d');
+    if (!context) return;
+    let time = 0;
+    context.scale(5, 5);
+
+    const color = function (x: number, y: number, r: number, g: number, b: number) {
+      context.fillStyle = `rgb(${r}, ${g}, ${b})`;
+      context.fillRect(x, y, 10, 10);
+    };
+    const R = function (x: number, y: number, time: number) {
+      return Math.floor(192 + 64 * Math.cos((x * x - y * y) / 300 + time));
+    };
+
+    const G = function (x: number, y: number, time: number) {
+      return Math.floor(192 + 64 * Math.sin((x * x * Math.cos(time / 4) + y * y * Math.sin(time / 3)) / 300));
+    };
+
+    const B = function (x: number, y: number, time: number) {
+      return Math.floor(
+        192 + 64 * Math.sin(5 * Math.sin(time / 9) + ((x - 100) * (x - 100) + (y - 100) * (y - 100)) / 1100),
+      );
+    };
+
+    const startAnimation = function () {
+      for (let x = 0; x <= 30; x++) {
+        for (let y = 0; y <= 30; y++) {
+          color(x, y, R(x, y, time), G(x, y, time), B(x, y, time));
+        }
+      }
+      time = time + 0.06;
+      window.requestAnimationFrame(startAnimation);
+    };
+
+    startAnimation();
+  }, [canvasRef]);
+
   return (
     <Layout>
       <Hero>
         <Row style={{ height: 'calc(100vh - 72px)' }} align="middle" justify="center">
           <Col>
+            <canvas
+              style={{ width: '100vw', height: '100vh' }}
+              ref={(r) => {
+                canvasRef.current = r ?? undefined;
+              }}
+            />
             <Typography>
               <Title level={1}>Take your marks to the stars!</Title>
             </Typography>
